5cbe2d76a9dfa51a57de9f112ce324eac2b0fb1b
sabinshrestha7065 <sstha7065@gmail.com>
2023-06-08 23:38:40 +0545

    add file and ssh file

---
+++ index.js
@@ -0,0 +1,16 @@
+const age = 12;
+
+if (age > 12) {
+  console.log("you can play the video game.");
+} else if(age < 23) {
+  console.log("you can play the pubg game.");
+}else if(age < 50){
+  console.log("you can play cards.")
+};
+
+
+function name( fullName, surName ){
+  console.log(`${fullName} ${surName}`);
+};
+
+name("manish", "shrestha");
