8aa8c777c98028291f964075bb2a1f0d0df680de
sabinshrestha7065 <sstha7065@gmail.com>
2023-06-11 22:24:33 +0545

    modify the file

--- index.js
+++ index.js
@@ -28,3 +28,43 @@ vehicleName.pop();
 
 // console.log(vehicleName);
 // alert(vehicleName);
+a = 12;
+b = 3;
+c = 13;
+if (a < b) {
+  console.log("this number is valid.");
+} else if (b < c) {
+  console.log("b is less then c");
+}
+
+const person = {
+  firstName: "manish",
+  lastName: "shrestha",
+  age: 25,
+  eyacolor: "black",
+};
+function name() {
+  console.log(this.firstName + " " + this.lastName);
+}
+name();
+
+
+const myObj1 ={
+  car:"lambo"
+}
+const myObj2 ={
+  name:"manish",
+  age :13,
+  favLanguage :"nepali"
+}
+
+Object.assign(myObj1,myObj2)
+
+console.log(myObj1)
+const colors = new Set();
+
+colors.add("red");
+colors.add("black");
+colors.add("yellow");
+colors.delete("red");
+console.log(colors.size); // print 2
