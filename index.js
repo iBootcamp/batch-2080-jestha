55d2ab6f63195a70b08ac85721519a5732f2a375
sabinshrestha7065 <sstha7065@gmail.com>
2023-06-18 00:24:12 +0545

    modify

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ name("manish", "shrestha");
 
 // break line
 
-console.log("iam \n manish \n shrestha");
+  //console.log("iam \n manish \n shrestha");
 
 // Array
 
@@ -44,28 +44,82 @@ const person = {
   eyacolor: "black",
 };
 function name() {
-  console.log(this.firstName + " " + this.lastName);
+ // console.log(this.firstName + " " + this.lastName);
 }
 name();
 
+const myObj1 = {
+  car: "lambo",
+};
+const myObj2 = {
+  name: "manish",
+  age: 13,
+  favLanguage: "nepali",
+};
 
-const myObj1 ={
-  car:"lambo"
-}
-const myObj2 ={
-  name:"manish",
-  age :13,
-  favLanguage :"nepali"
-}
+Object.assign(myObj1, myObj2);
 
-Object.assign(myObj1,myObj2)
-
-console.log(myObj1)
+console.log(myObj1);
 const colors = new Set();
 
 colors.add("red");
 colors.add("black");
 colors.add("yellow");
 colors.delete("red");
-console.log(colors.size); // print 2
+// console.log(colors.size); // print 2
+
+//  practise  of june 17 Day saturday
+
+// switch statement
+
+//if-else
+if (age >= 14 && age < 19) {
+  status = "Eligible";
+} else {
+  status = "not eligible";
+}
+
+///console.log(status)
+
+switch (new Date().getDay()) {
+  case 6:
+    text = "saturday";
+    break;
+  case 0:
+    text = "sunday";
+    break;
+  default:
+    text = "whatever";
+}
+// console.log(text);
+
+//primtives data types
+
+var age1 = 21; // typeOf number
+var name1 = "misss"; // typeOf string
+var name1 = {
+  first: "hello",
+  last: "world",
+}; // typeOf object
+var truth = false; // typeof boolen
+var sheets = ["HTML", "CSS", "JS"]; // typeOf array
+var a; // typeOF undefined
+var a = null; // typeOf null
+
+// loops
+for ( i = 0; i >5; i++){
+  console.log(i)
+};
+
+// line break and n't
+var esc = " I don\'t know. \n what about javascript?";
+ //console.log(esc);
+
+
+const nepaliLanguage = "nepali";
+
+const smallLetter = nepaliLanguage.toLowerCase();
+const capitalLetter = nepaliLanguage.toUpperCase();
+
+ // console.log(capitalLetter);
 
